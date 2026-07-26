10be9b6713ba9db75b34bcc2f1c564147995f192
Melissa Huamán <melissa17huaman@gmail.com>
2026-07-26 17:06:05 +0000

    modificado

--- src/app.py
+++ src/app.py
@@ -2,17 +2,14 @@ import streamlit as st
 
 from organ_apps import cerebro_app
 from organ_apps import higado_app
-from organ_apps import pulmon_app
-from organ_apps import retina_app
-
 
 st.set_page_config(
-    page_title="Medical Imaging Diagnosis",
+    page_title="MedImage Diagnosis",
     page_icon="🩺",
     layout="centered"
 )
 
-st.title("🩺 Medical Imaging Diagnosis")
+st.title("🩺 MedImage Diagnosis")
 
 st.write(
     """
@@ -30,9 +27,7 @@ option = st.sidebar.selectbox(
     "Selecciona el estudio",
     [
         "🧠 Cerebro",
-        "🟤 Hígado",
-        "🫁 Pulmones",
-        "👁️ Retinopatía diabética"
+        "🟤 Hígado"
     ]
 )
 
@@ -41,9 +36,3 @@ if option == "🧠 Cerebro":
 
 elif option == "🟤 Hígado":
     higado_app.run()
-
-elif option == "🫁 Pulmones":
-    pulmon_app.run()
-
-elif option == "👁️ Retinopatía diabética":
-    retina_app.run()
